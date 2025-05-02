b16ff4ba32e1cd58c0991a123e8d9622951d7840
Aditya Subramanian <aditarun2008@gmail.com>
2025-05-01 23:21:55 -0400

    update npm build script

--- scripts/build_npm.ts
+++ scripts/build_npm.ts
@@ -16,17 +16,19 @@ await build({
     name: name,
     version: version,
     description: "ORCID API client",
-    keywords: ["orcid", "api"],
     license: license,
-    dependencies: {
-      "@zod/mini": "^4.0.0-beta.20250424T163858",
-    },
+    keywords: ["orcid", "api"],
     repository: {
       type: "git",
-      url: "git+https://github.com/adiSupe94/orcid.git",
+      url: "git+https://github.com/adiSuper94/orcid.git",
     },
     bugs: {
       url: "https://github.com/adiSuper94/orcid/issues",
+    },
+    homepage: "https://github.com/adiSuper94/orcid#readme",
+    author: "Aditya Subramanian",
+    dependencies: {
+      "@zod/mini": "^4.0.0-beta.20250424T163858",
     },
   },
   postBuild() {
